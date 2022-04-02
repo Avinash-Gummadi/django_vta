ea38b5bcdcfde84a26ac4c583e98b4d7b6f482c6
Avinash-Gummadi <avinash18bcs@iiitkottayam.ac.in>
2022-04-03 01:25:01 +0530

    quiz

--- vta/admin.py
+++ vta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import UploadQuestion
 # Register your models here.
+class UploadQuestionAdmin(admin.ModelAdmin):
+    list_filter = ['subject', 'answer']
+    list_display = ['id','subject','answer']
+    class Meta:
+        model = UploadQuestion
+admin.site.register(UploadQuestion, UploadQuestionAdmin)
--- vta/models.py
+++ vta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+class UploadQuestion(models.Model):
+    subjects = (('Programming','Programming'),('DBMS','DBMS'),('Oops','Oops'),('Python','Python'))
+    optionsList = (('optionA','A'),('optionB','B'),('optionC','C'),('optionD','D'))
+    subject = models.CharField(choices = subjects, max_length=100, blank=False)
+    question = models.TextField(unique=False, blank=False)
+    optionA = models.CharField(max_length=50, blank=False)
+    optionB = models.CharField(max_length=50, blank=False)
+    optionC = models.CharField(max_length=50, blank=False)
+    optionD = models.CharField(max_length=50, blank=False)
+    answer = models.CharField(choices = optionsList, max_length=50, blank=False)
